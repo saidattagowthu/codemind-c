5e0a8d74985af476189ea9c96144a8e05cbfd8bf
saidattagowthu <147487732+saidattagowthu@users.noreply.github.com>
2023-11-06 14:46:22 +0530

    #include<stdio.h> int main() { 	int a,b,c,d=0; 	scanf("%d",&a); 	c=a; 	while(a!=0) 	{ 		b=a%10; 		d=d*10+b; 		a=a/10; 			}
    		    if(c==d){
    			printf("True");}
    			else
    			{
    			printf("False");
    		}
    		
    }

---
+++ Palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d=0;
+	scanf("%d",&a);
+	c=a;
+	while(a!=0)
+	{
+		b=a%10;
+		d=d*10+b;
+		a=a/10;
+			}
+		
+		    if(c==d){
+			printf("True");}
+			else
+			{
+			printf("False");
+		}
+		
+}
