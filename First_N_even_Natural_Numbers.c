7255c736833f65cd5a1f573ff0cd2d94c72b91f0
saidattagowthu <147487732+saidattagowthu@users.noreply.github.com>
2023-12-06 18:29:46 +0530

    #include<stdio.h> int main() {     int a,i,s=0;     scanf("%d",&a);     for(i=a*2;i>1;i--)     {
            if(i%2==0)
            {
          printf("%d ",i);
            }
        }
         
    }

---
+++ First_N_even_Natural_Numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,s=0;
+    scanf("%d",&a);
+    for(i=a*2;i>1;i--)
+    {
+      
+        if(i%2==0)
+        {
+      printf("%d ",i);
+        }
+    }
+     
+}
