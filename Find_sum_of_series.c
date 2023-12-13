d21bbc65229a538af28e5c8cec25460eb6e897e8
saidattagowthu <147487732+saidattagowthu@users.noreply.github.com>
2023-12-13 08:32:48 +0530

    #include<stdio.h> int main() {     float a,b,c=0,i;     scanf("%f",&a);     for(i=1;i<=a;i++)     {         b=1/i;         c=c+b;
        }
        printf("%.2f",c);
    }

--- Find_sum_of_series.c
+++ Find_sum_of_series.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    float a,b,i,s=0;
+    float a,b,c=0,i;
     scanf("%f",&a);
     for(i=1;i<=a;i++)
     {
-       b=1/(i);
-       s=s+b;
-       
+        b=1/i;
+        c=c+b;
+        
     }
-    printf("%.2f ",s);
+    printf("%.2f",c);
 }
