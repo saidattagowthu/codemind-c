95184b82df549951122fadc82bafbfbbf359c9f6
saidattagowthu <147487732+saidattagowthu@users.noreply.github.com>
2023-12-13 21:21:49 +0530

    #include<stdio.h> int main() {     int a,b,gcd,lcm,n,r,d;     scanf("%d%d",&a,&b);     n=(a>b)?a:b;     d=(a<b)?a:b;     r=n%d;     while(r!=0)     {         n=d;         d=r;         r=n%d;
        }
        gcd=d;
       
        printf("%d",gcd);
    }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,gcd,lcm,n,r,d;
+    scanf("%d%d",&a,&b);
+    n=(a>b)?a:b;
+    d=(a<b)?a:b;
+    r=n%d;
+    while(r!=0)
+    {
+        n=d;
+        d=r;
+        r=n%d;
+        
+    }
+    gcd=d;
+   
+    printf("%d",gcd);
+}
