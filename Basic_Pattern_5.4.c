d185f744820a8c7112c2c8ce923e8b0ba8f1bc3e
saidattagowthu <147487732+saidattagowthu@users.noreply.github.com>
2023-12-31 17:09:20 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=n;j++)         {             if(j>i)             printf("*");             else             printf("%d",j);         }         printf(" ");     } }

---
+++ Basic_Pattern_5.4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j>i)
+            printf("*");
+            else
+            printf("%d",j);
+        }
+        printf("
+");
+    }
+}
