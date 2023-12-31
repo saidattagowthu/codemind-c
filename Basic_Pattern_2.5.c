ffa3ec043ac58d6dc808b2d2e4cc55870ccbdea4
saidattagowthu <147487732+saidattagowthu@users.noreply.github.com>
2023-12-31 17:01:35 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=n-i+1;j++)         {             printf("%c ",i+64);         }         printf(" ");         }
    }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n-i+1;j++)
+        {
+            printf("%c ",i+64);
+        }
+        printf("
+");
+        }
+    
+}
