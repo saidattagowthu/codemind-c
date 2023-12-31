6f63a656daa19a18812e21fd371e0922ce83a475
saidattagowthu <147487732+saidattagowthu@users.noreply.github.com>
2023-12-31 17:15:37 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)       {         if(i==j||i+j==n-1)             printf("%c",i+65);             else             printf(" ");       }         printf(" ");     }  }

---
+++ Pattern_8.4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+      {
+        if(i==j||i+j==n-1)
+            printf("%c",i+65);
+            else
+            printf(" ");
+      }
+        printf("
+");
+    }
+ }
