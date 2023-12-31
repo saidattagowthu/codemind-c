7212e73f8ae3300a00fac04b902556ff1a8a9bab
saidattagowthu <147487732+saidattagowthu@users.noreply.github.com>
2023-12-31 17:12:41 +0530

    #include<stdio.h> int main() {     int i,j,n,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {       for(j=n-1;j>=i;j--)     {         printf(" ");     }     for(k=i;k>=1;k--)     {         printf("%c ",i+64);     }     printf(" ");     }
        }

---
+++ Pattern_7.4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,k;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+      for(j=n-1;j>=i;j--)
+    {
+        printf(" ");
+    }
+    for(k=i;k>=1;k--)
+    {
+        printf("%c ",i+64);
+    }
+    printf("
+");
+    }
+ 
+    }
